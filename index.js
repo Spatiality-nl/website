0624e6ee6f0d5b7f23f97246dea94089328f9436
Kamieljv <kamiel.verhelst@proton.me>
2025-01-02 16:25:37 +0100

    Moved resources, changed font, implemented bootstrap

--- index.js
+++ index.js
@@ -7,26 +7,27 @@ import { getFresnelMat } from "./src/js/getFresnelMat.js";
 const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 1000);
-camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(50, w / h, 0.1, 1000);
+camera.position.z = 2.5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
-document.body.appendChild(renderer.domElement);
-// THREE.ColorManagement.enabled = true;
+document.getElementById('threejs-container').appendChild(renderer.domElement);
+THREE.ColorManagement.enabled = true;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
 const earthGroup = new THREE.Group();
+earthGroup.position.x = 1;
 earthGroup.rotation.z = -23.4 * Math.PI / 180;
 scene.add(earthGroup);
-new OrbitControls(camera, renderer.domElement);
+// new OrbitControls(camera, renderer.domElement);
 const detail = 12;
 const loader = new THREE.TextureLoader();
 const geometry = new THREE.IcosahedronGeometry(1, detail);
 const material = new THREE.MeshPhongMaterial({
-  map: loader.load("./textures/00_earthmap1k.jpg"),
-  specularMap: loader.load("./textures/02_earthspec1k.jpg"),
-  bumpMap: loader.load("./textures/01_earthbump1k.jpg"),
+  map: loader.load("./resources/textures/00_earthmap1k.jpg"),
+  specularMap: loader.load("./resources/textures/02_earthspec1k.jpg"),
+  bumpMap: loader.load("./resources/textures/01_earthbump1k.jpg"),
   bumpScale: 0.04,
 });
 // material.map.colorSpace = THREE.SRGBColorSpace;
@@ -34,18 +35,18 @@ const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
 
 const lightsMat = new THREE.MeshBasicMaterial({
-  map: loader.load("./textures/03_earthlights1k.jpg"),
+  map: loader.load("./resources/textures/03_earthlights1k.jpg"),
   blending: THREE.AdditiveBlending,
 });
 const lightsMesh = new THREE.Mesh(geometry, lightsMat);
 earthGroup.add(lightsMesh);
 
 const cloudsMat = new THREE.MeshStandardMaterial({
-  map: loader.load("./textures/04_earthcloudmap.jpg"),
+  map: loader.load("./resources/textures/04_earthcloudmap.jpg"),
   transparent: true,
   opacity: 0.8,
   blending: THREE.AdditiveBlending,
-  alphaMap: loader.load('./textures/05_earthcloudmaptrans.jpg'),
+  alphaMap: loader.load('./resources/textures/05_earthcloudmaptrans.jpg'),
   // alphaTest: 0.3,
 });
 const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
